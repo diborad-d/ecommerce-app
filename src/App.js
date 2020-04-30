0044de94c5bef997be0bf8f329544f9409aad959
Dibora D <diborad@yahoo.com>
2020-04-30 17:06:28 -0500

    created components and updated package.json dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { realpathSync } from "fs";
 
 function App() {
   return (
-    
+    //REACT.GRAGMENT simulates html elemets
+    <React.Fragment>
+      <h3>hello from app.js</h3>
+    </React.Fragment>
   );
 }
 
