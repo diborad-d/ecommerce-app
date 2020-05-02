7d6c9a462caf638f6f40a69349e980ffb642a305
Dibora D <diborad@yahoo.com>
2020-05-02 18:13:01 -0500

    added products page as home dashboard and add nav background-primary

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,11 +4,14 @@ import { Link } from "react-router-dom";
 export default class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar=example-sm navbar-dark px-sm-5">
+      <nav className="navbar navbar=example-sm bg-primary navbar-dark px-sm-5">
         <Link to="/">
-          <i class="fas fa-store-alt fa-3x"></i>
-          {/* <img src="/images/logo.svg" alt="Logo" className="navbar-brand" /> */}
+          <i class="fas fa-store-alt fa-2x"></i>
         </Link>
+      <ul className="navbar-nav align-items-center">
+        <li className="nav-item" ml-5></li>
+        <Link to="/" className="nav-link"> Products</Link>
+      </ul>
       </nav>
     );
   }
