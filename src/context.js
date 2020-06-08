72b6076889e44ffe45fb5680aefe495d403e6e32
Dibora D <diborad@yahoo.com>
2020-06-07 23:44:11 -0500

    imported data to context

--- src/context.js
+++ src/context.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-
+import { storeProduct, detailpProduct } from "./data";
 const ProductContext = React.createContext();
 class ProductProvider extends Component {
   render() {
-    return(
-    <ProductContext.Provider value="hello from context">
-      {/* using Context: since provider is gonna be sitting on top of our component tree,we need to return all the childern that are gonna be in the app*/}
-      {/* right now we are gonna return the children that fall under this componet */}
-      {this.props.children}
-    </ProductContext.Provider>
+    return (
+      <ProductContext.Provider value="hello from context">
+        {/* using Context: since provider is gonna be sitting on top of our component tree,we need to return all the childern that are gonna be in the app*/}
+        {/* right now we are gonna return the children that fall under this componet */}
+        {this.props.children}
+      </ProductContext.Provider>
     );
   }
 }
